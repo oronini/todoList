f64805b4312ef084ac2176205eecd3e314b3c12a
mikami koji <wmd147wmd@gmail.com>
2022-06-11 21:42:36 +0900

    修正

--- main.js
+++ main.js
@@ -1,77 +1,69 @@
-import './style.css'
+// import './style.css'
 
-  const form = document.getElementById('form');
-  const input = document.getElementById('input');
-  const ul = document.getElementById('ul');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
 
-  const todos = JSON.parse(localStorage.getItem('todos'));
+const todos = JSON.parse(localStorage.getItem("todos"));
 
-  if (todos) {
-    todos.forEach(todo => {
-      add(todo);
-    })
-  }
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();    
-    add();
+if (todos) {
+  todos.forEach((todo) => {
+    add(todo);
   });
+}
 
-  function add(todo) {
-    let todoTxte = input.value;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  add();
+});
 
-    if (todo) {
-      todoTxte =todo.text;
-    }
-    
-    if (todoTxte) {
-      const li = document.createElement("li");
-      li.innerText = todoTxte
-      li.classList.add("list-group-item");
+function add(todo) {
+  let todoText = input.value;
 
-      if (todo && todo.completed) {
-        li.classList.add("text-decoration-line-through");
-      }
-      
-      li.addEventListener('contextmenu' , (e) => {
-        e.preventDefault();
-        li.remove();
-        saveDate();
-      });
-
-      li.addEventListener('click', () => {
-        li.classList.toggle("text-decoration-line-through")
-        saveDate();
-      });
-
-      li.addEventListener('dblclick', () => {
-        console.log("dblclick");
-      })
-    
-
-      ul.appendChild(li);
-      input.value = "";
-      saveDate();
-    }
-  };
-
-
-  function saveDate () {
-    const lists = document.querySelectorAll("li");
-    let todos = [];
-
-    lists.forEach(list => {
-      let todo = {
-        text: list.innerText,
-        completed: list.classList.contains("text-decoration-line-through")
-      };
-      todos.push(todo);
-    });
-    localStorage.setItem('todos', JSON.stringify(todos));
+  if (todo) {
+    todoText = todo.text;
   }
 
+  if (todoText) {
+    const li = document.createElement("li");
+    li.innerText = todoText;
+    li.classList.add("list-group-item");
 
-  document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+    if (todo && todo.completed) {
+      li.classList.add("text-decoration-line-through");
+    }
+
+    li.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      li.remove();
+      saveDate();
+    });
+
+    li.addEventListener("click", () => {
+      li.classList.toggle("text-decoration-line-through");
+      saveDate();
+    });
+
+    li.addEventListener("dblclick", (e) => {
+      console.log(li.innerText);
+    });
+
+    ul.appendChild(li);
+    input.value = "";
+    saveDate();
+  }
+}
+
+function saveDate() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+
+  lists.forEach((list) => {
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains("text-decoration-line-through"),
+    };
+    todos.push(todo);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
